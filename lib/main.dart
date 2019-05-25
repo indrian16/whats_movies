f285c644fcbed3bab73c4051abacf382420d5dab
rg16 <indrian16@outlook.co.id>
2019-05-25 15:21:38 +0800

    init project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'What Movies',
+      theme: ThemeData.light(),
+      home: HomeScreen(),
+    );
+  }
+}
